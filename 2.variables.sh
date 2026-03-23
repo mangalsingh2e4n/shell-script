b47b7e91ce0ef67938d9a57142a34c3724410825
Mangal Singh <mangalsingh2e4n@gmail.com>
2026-03-23 22:17:22 +0530

    variables

--- 2.variables.sh
+++ 2.variables.sh
@@ -4,3 +4,15 @@ echo "ramesh::hii re gandu!"
 echo "suresh::bol be"
 echo "ramesh::kya karra?"
 echo "suresh::kuch nahi"
+
+
+#using variables for conversation
+
+FRIEND1=miya
+FRIEND2=bhai
+
+echo "$FRIEND1::hii re gandu!"
+echo "$FRIEND2::bol be"
+echo "$FRIEND1::kya karra?"
+echo "$FRIEND2::kuch nahi"
+
