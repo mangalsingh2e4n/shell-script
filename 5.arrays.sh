e31806ad984b7c93d1bf1da95143882cfe03d7cb
Mangal Singh <mangalsingh2e4n@gmail.com>
2026-03-23 23:50:58 +0530

    data

--- 5.arrays.sh
+++ 5.arrays.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MOVIES=("gunda","munda","genda","lenda")
+MOVIES=("gunda" "munda" "genda" "lenda")
 
 echo "The second movie name is: ${MOVIES[1]}"
 echo "The second movie name is: ${MOVIES[@]}"
