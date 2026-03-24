bbdb51e7f73e6792a056d6cae33bdb275efd76d5
Mangal Singh <mangalsingh2e4n@gmail.com>
2026-03-24 12:31:12 +0530

    conditions

--- 8.intecon.sh
+++ 8.intecon.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 NUM=$1
+TIMESTAMP=$TIME
+    echo "the script executed at::$TIMESTAMP"
+LOGS=$LOG
+    echo "the log files is ::$LOGS"
 if [ $NUM -gt 100 ]
 then
     echo "then greater than 100"
